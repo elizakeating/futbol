c9e3dac0bcea5b6068acb4c53634926461e9bbd3
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-03 20:32:55 -0500

    refactor: verifying tests accurate

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,6 +1,7 @@
 require_relative 'game_statistics.rb'
 require_relative 'league_statistics.rb'
 require_relative 'season_statistics.rb'
+require 'csv'
 
 class StatTracker
   attr_reader :game_statistics, :season_statistics, :league_statistics
@@ -11,9 +12,9 @@ class StatTracker
   end
 
   def self.from_csv(locations)
-    games_data = CSV.read(locations[:games], headers: true, header_converters: :symbol) 
-    teams_data = CSV.read(locations[:teams], headers: true, header_converters: :symbol) 
-    game_teams_data = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol) 
+    games_data = CSV.read locations[:games], headers: true, header_converters: :symbol
+    teams_data = CSV.read locations[:teams], headers: true, header_converters: :symbol
+    game_teams_data = CSV.read locations[:game_teams], headers: true, header_converters: :symbol
     new(games_data, teams_data, game_teams_data)
   end
 
