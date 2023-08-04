b99dc382abcc7cfc27474ad0b9b39b36e83a9341
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-03 19:40:27 -0500

    refactor: refactored class to accommodate stat tracker update

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -1,63 +1,54 @@
 require 'csv'
 class GameStatistics
-  attr_reader :locations,
-              :teams_data,
-              :game_data,
-              :game_team_data
-
-  def initialize(locations)
-    @locations = locations
-    @game_data = CSV.open locations[:games], headers: true, header_converters: :symbol
-    @teams_data = CSV.open locations[:teams], headers: true, header_converters: :symbol
-    @game_team_data = CSV.open locations[:game_teams], headers: true, header_converters: :symbol  
+  def initialize
   end
 
   def percentage_calculator(portion, whole)
     percentage_return = (portion / whole).round(2)
   end
 
-  def highest_total_score
+  def highest_total_score(location)
     highest_total_score = 0
-    @game_data.each do |row|
+    location.each do |row|
     total_score = row[:away_goals].to_i + row[:home_goals].to_i #away_goals + home_goals
     highest_total_score = total_score if total_score > highest_total_score
     end
     highest_total_score
   end
 
-  def lowest_total_score
+  def lowest_total_score(location)
     lowest_total_score = nil
-    @game_data.each do |row|
+    location.each do |row|
     total_score = row[:away_goals].to_i + row[:home_goals].to_i #away_goals + home_goals
       lowest_total_score = total_score if lowest_total_score == nil || total_score < lowest_total_score
     end
     lowest_total_score
   end
   
-  def percentage_home_wins  
+  def percentage_home_wins(location)
     total_games = 0
     home_team_wins = 0
-    @game_data.each do |row|
+    location.each do |row|
       total_games += 1.0
       home_team_wins += 1.0 if row[:home_goals].to_f > row[:away_goals].to_f
     end
     percentage_calculator(home_team_wins, total_games)
   end
 
-  def percentage_visitor_wins  
+  def percentage_visitor_wins(location)
     total_games = 0
     visitor_team_wins = 0
-    @game_data.each do |row|
+    location.each do |row|
       total_games += 1.00
       visitor_team_wins += 1.00 if row[:away_goals].to_f > row[:home_goals].to_f
     end
     percentage_calculator(visitor_team_wins, total_games)
   end
 
-  def percentage_ties
+  def percentage_ties(location)
     total_games = 0
     tied_games = 0
-    @game_data.each do |row|
+    location.each do |row|
       total_games += 1.00
       tied_games += 1.00 if row[:away_goals].to_f == row[:home_goals].to_f
     end
