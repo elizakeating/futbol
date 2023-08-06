77ed7fbb4b7140ce6c195483db45190f51ca9c17
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-08-05 19:49:52 -0600

    Added remaining game methods and league methods, made changes to league to change to float

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -37,4 +37,60 @@ RSpec.describe StatTracker do
   it "#percentage_ties" do
     expect(@stat_tracker.percentage_ties).to eq 0.20
   end
+
+  it "returns a hash with season names as keys and count of games as values" do
+    expected = {
+      "20122013"=>806,
+      "20162017"=>1317,
+      "20142015"=>1319,
+      "20152016"=>1321,
+      "20132014"=>1323,
+      "20172018"=>1355
+    }
+    expect(@stat_tracker.count_of_games_by_season).to eq expected
+  end
+
+  it "returns average number of goals scored in a game across all seasons including both home and away goals" do
+    expect(@stat_tracker.average_goals_per_game).to eq(4.22)
+  end
+
+  it "returns average number of goals scored in a game in that season" do
+    expected = {
+      "20122013"=>4.12,
+      "20162017"=>4.23,
+      "20142015"=>4.14,
+      "20152016"=>4.16,
+      "20132014"=>4.19,
+      "20172018"=>4.44
+    }
+    expect(@stat_tracker.average_goals_by_season).to eq expected
+  end
+
+  it "#count_of_teams" do
+    expect(@stat_tracker.count_of_teams).to eq 32
+  end
+
+  it "#best_offense" do
+    expect(@stat_tracker.best_offense).to eq "Reign FC"
+  end
+
+  it "#worst_offense" do
+    expect(@stat_tracker.worst_offense).to eq "Utah Royals FC"
+  end
+
+  it "#highest_scoring_visitor" do
+    expect(@stat_tracker.highest_scoring_visitor).to eq "FC Dallas"
+  end
+
+  it "#highest_scoring_home_team" do
+    expect(@stat_tracker.highest_scoring_home_team).to eq "Reign FC"
+  end
+
+  it "#lowest_scoring_visitor" do
+    expect(@stat_tracker.lowest_scoring_visitor).to eq "San Jose Earthquakes"
+  end
+
+  it "#lowest_scoring_home_team" do
+    expect(@stat_tracker.lowest_scoring_home_team).to eq "Utah Royals FC"
+  end
 end 
