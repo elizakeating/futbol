998a4ff881385ab9e0ebf2a784358ba58ecce7d2
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-05 19:49:24 -0500

    stat tracker spec tests, game stats

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -105,4 +105,24 @@ class GameStatistics
 
     average_goals_by_season
   end
+
+  it "#highest_total_score" do
+    expect(@stat_tracker.highest_total_score).to eq 11
+  end
+
+  it "#lowest_total_score" do
+    expect(@stat_tracker.lowest_total_score).to eq 0
+  end
+
+  it "#percentage_home_wins" do
+    expect(@stat_tracker.percentage_home_wins).to eq 0.44
+  end
+
+  it "#percentage_visitor_wins" do
+    expect(@stat_tracker.percentage_visitor_wins).to eq 0.36
+  end
+
+  it "#percentage_ties" do
+    expect(@stat_tracker.percentage_ties).to eq 0.20
+  end
 end
