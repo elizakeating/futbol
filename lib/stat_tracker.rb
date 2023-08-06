e46e6b692b8590703c376228733b2a10533dfbc0
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-05 19:56:45 -0500

    updated stat tracker and spec for game stat methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -9,9 +9,9 @@ class StatTracker
               :league_statistics
   def initialize(locations)
     @locations = locations
-    @game_data = CSV.read locations[:games], headers: true, header_converters: :symbol
-    @teams_data = CSV.read locations[:teams], headers: true, header_converters: :symbol
-    @game_team_data = CSV.read locations[:game_teams], headers: true, header_converters: :symbol  
+    @game_statistics = GameStatistics.new(locations) 
+    @league_statistics = LeagueStatistics.new(locations)
+    @season_statistics = SeasonStatistics.new(locations)
   end
 
   def self.from_csv(locations)
