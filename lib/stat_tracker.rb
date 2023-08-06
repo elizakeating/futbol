0cd005ee7025b805380b2c1b019c64715cfb38d7
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2023-08-05 20:28:16 -0400

    Stat_Tracker Setup

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,3 +1,7 @@
 class StatTracker
-  
-end
+  def initialize(locations)
+    @locations = locations
+    @game_data = CSV.read locations[:games], headers: true, header_converters: :symbol
+    @teams_data = CSV.read locations[:teams], headers: true, header_converters: :symbol
+    @game_team_data = CSV.read locations[:game_teams], headers: true, header_converters: :symbol  
+  end
