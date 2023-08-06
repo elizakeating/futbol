ef4fcdf2909d42ecc03868fd8e6bfbdc3960c5a5
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-05 19:48:30 -0500

    stat_tracker_game_stat_methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -17,4 +17,24 @@ class StatTracker
   def self.from_csv(locations)
     StatTracker.new(locations)
   end
+
+  def highest_total_score
+    @game_statistics.highest_total_score
+  end
+
+  def lowest_total_score
+    @game_statistics.lowest_total_score
+  end
+
+  def percentage_home_wins
+    @game_statistics.percentage_home_wins
+  end
+
+  def percentage_visitor_wins
+    @game_statistics.percentage_visitor_wins
+  end
+
+  def percentage_ties
+    @game_statistics.percentage_ties
+  end
 end 
