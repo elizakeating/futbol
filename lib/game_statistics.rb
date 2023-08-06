a33a7c8c0ca2515b9f289c92aecf44471e941cb2
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-05 19:57:04 -0500

    accidental updated deleted

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -9,7 +9,7 @@ class GameStatistics
     @locations = locations
     @game_data = CSV.read locations[:games], headers: true, header_converters: :symbol
     @teams_data = CSV.read locations[:teams], headers: true, header_converters: :symbol
-    @game_team_data = CSV.read locations[:game_teams], headers: true, header_converters: :symbol  
+    @game_team_data = CSV.rea locations[:game_teams], headers: true, header_converters: :symbol  
   end
 
   def percentage_calculator(portion, whole)
